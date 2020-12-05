271add8637b492e9c0e573af0206d9daa2f5bb62
aneenajohn <75213145+aneenajohn@users.noreply.github.com>
2020-12-05 20:39:24 +0530

    updated eventListener for click

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
-var btnTranslate = document.querySelector("#btn-translate");
 // creating a ref for button from html in js
-console.log(btnTranslate)
+var btnTranslate = document.querySelector("#btn-translate");
+
+// 1) part1
+// // console.log(btnTranslate)
+// this is to chk in browser console
+// 1.right click and save <button>....</button> as global variable.
+// 2.temp1---> becoomes a global variable.
+// 3.you can use  this to contrl ur html from js , eg: temp1.style.backgroundColor ="red"
+
+// 2)part2
+// btnTranslate.addEventListener("click", function clickEventHandler(){
+//    console.log("clicked") ;
+// }
+// )
+
+// 3)part3
+// Call back--->call me back -when this event happens call me back;
+// when click happens call me--me ref to clickEventHandler()
+
+var txtInput = document.querySelector("#txt-input");
+console.log(txtInput);
+
+btnTranslate.addEventListener("click", clickHandler())
+
+function clickHandler() {
+    console.log("you clicked");
+    console.log("input",txtInput.value);
+};
